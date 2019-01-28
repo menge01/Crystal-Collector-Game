d8d5b3b1f02c01988f948174c8a3f84fdf3af4d6
menge01 <menge0882@yahoo.com>
2019-01-28 11:25:07 -0500

    change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,79 @@
 $(document).ready(function(){
 
     var random = Math.floor(Math.random()*102 + 19)
-     
     $('#matchThis').text(random);
     var num1 = Math.floor(Math.random()*12+1);
     var num2 = Math.floor(Math.random()*12+1);
     var num3 = Math.floor(Math.random()*12+1);
     var num4 = Math.floor(Math.random()*12+1);
-
-    var playerTotal = 0;
     var wins =0;
     var losses = 0;
-
-
+    var userTotalScore = 0;
     $("wins").text(wins);
     $("losses").text(losses);
+      function playgame(){
+          if (userTotalScore === random){
+            wins++;
+            $("#wins").text(wins);
+            alert("winner! You won!");
+            random = Math.floor(Math.random()*102 + 9);
+            $('#matchThis').text(random);
+            userTotalScore = 0;
+            $('#current-score-counter').text(userTotalScore);
+            restart();
+          
+           } else if (userTotalScore > random){
+            losses++;
+            $("#losses").text(losses);
+            alert("Sorry! You lose!");
+            random = Math.floor(Math.random()*102 + 9); 
+            $('#matchThis').text(random);
+
+           userTotalScore = 0;
+            $('#current-score-counter').text(userTotalScore);
+            restart();
+
+           }
+        }
+
     
    function restart(){
-       random = Math.floor(Math.random()*102 + 9);
-       console.log(random);
-       $('#matchThis').text(random);
        num1 = Math.floor(Math.random()* 12+1);
        num2 = Math.floor( Math.random()*12+1);
        num3 = Math.floor(Math.random() *12+1);
        num4 = Math.floor(Math.random()*12+1);
-       playerTotal = 0;
-       $('#totalScore').text(playerTotal);
-       
-   }
+      
+     }
 
-function winner(){
-    alert("winner! You won!");
-    wins++;
-    $('#wins').text(wins);
-    restart();
-}
-function losser(){
-    alert("Sorry! You lose!");
-    losses++;
-    $("losses").text(losses);
-    restart()
-}
+   $('.red').on('click',function(){
+        userTotalScore +=num1;
+       $('#current-score-counter').text(userTotalScore);
+       console.log("New playerTotal= " + userTotalScore);
+       playgame();
 
-
-
-
-    $('.red').on('click',function(){
-        
-        playerTotal = playerTotal + num1;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-
-        if(playerTotal == random){
-
-            winner();
-        }
-        else if (playerTotal > random){
-            losser();
-        }
-        
-        
-    })
+        })
 
     $('.blue').on('click',function(){
-        playerTotal = playerTotal + num2;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal);
-
-      if(playerTotal == random){
-          winner();
-      }
-      else if(playerTotal > random){
-          losser();
-      }
-      
-
-        
-    })
+        userTotalScore +=num2;
+        $('#current-score-counter').text(userTotalScore);
+        console.log("New playerTotal= " + userTotalScore);
+        playgame();
+    
+      })
     $('.yellow').on('click',function(){
-        playerTotal = playerTotal + num3;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if(playerTotal == random){
-            winner();
-        }
-        else if(playerTotal > random){
-            losser();
-
-        }
-        
-        
-    })
+        userTotalScore +=num3;
+        $('#current-score-counter').text(userTotalScore);
+        console.log("New playerTotal= " + userTotalScore);
+        playgame();
+       
+     })
     $('.green').on ('click',function(){
+      
+        userTotalScore +=num4;
+        $('#current-score-counter').text(userTotalScore);
+        console.log("New playerTotal= " + userTotalScore);
+        playgame();
         
-        playerTotal = playerTotal + num4;
-        console.log("New playerTotal= " + playerTotal);
-        $('#totalScore').text(playerTotal);
-        if(playerTotal == random){
-            winner();
-        }
-        else if(playerTotal > random){
-            losser();
-        }
         
         
     })
